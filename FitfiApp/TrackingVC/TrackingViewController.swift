c1db6d2f81f8a9ce41378278ec254761badde4eb
ton1 <macintosh711@gmail.com>
2018-08-13 14:56:28 -0400

    Monday Regular Commit

---
+++ FitfiApp/TrackingVC/TrackingViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TrackingViewController.swift
+//  FitfiApp
+//
+//  Created by YAN YU on 2018-08-13.
+//  Copyright © 2018 Fitfi. All rights reserved.
+//
+
+import UIKit
+
+class TrackingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
