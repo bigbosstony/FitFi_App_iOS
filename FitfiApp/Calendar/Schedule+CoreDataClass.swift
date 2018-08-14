e32d08cd770eee597357e3c954ff8853413585d6
Harsh_iOS <harsh.hbs@gmail.com>
2018-08-14 11:19:57 -0400

    Calendar partially working

---
+++ FitfiApp/Calendar/Schedule+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Schedule+CoreDataClass.swift
+//  
+//
+//  Created by Harsh_Dev on 13/08/18.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Schedule)
+public class Schedule: NSManagedObject {
+
+}
